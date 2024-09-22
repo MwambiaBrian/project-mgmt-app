c2c704262c45faf0edd3ecc7f84c40cee2d32204
MwambiaBrian <brianmwambia4@gmail.com>
2024-09-22 21:20:04 +0300

    deploy

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import { graphqlHTTP } from "express-graphql";
 import schema from "./schema/schema.js";
 import connectDB from "./config/db.js";
+import path from "path";
 
 dotenv.config();
 
@@ -12,6 +13,21 @@ connectDB();
 const port = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
+
+//--------------------------Deployment--------------------------
+const __dirname1 = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "client/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "client", "dist", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("Hello from server");
+  });
+}
+
+//--------------------------Deployment---------------------------
 //GraphQL endpoint
 app.use(
   "/graphql",
